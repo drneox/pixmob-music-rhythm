86a2a4583f1ea99f2b06cfe923810eb952f77515
carlos <cganozap@gmail.com>
2022-11-27 01:26:45 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# pixmob-music-rhythm
-project for sync PixMob wristbands with the music
+
+# PixMob Music Rhythm
+
+Sync your PixMob wristbands with the music :notes: :sound: :dancer: using an esp3266 controller.
+
+- The IR codes used in this project were obtained from:  [https://github.com/danielweidman/flipper-pixmob-ir-codes.](https://github.com/danielweidman/flipper-pixmob-ir-codes.)
+
+https://user-images.githubusercontent.com/1235287/204122329-c0079990-3263-4185-87a6-407cd7c081b8.mp4
+
+
+## Components :white_check_mark:
+
+ - KY-038 Sensor.
+ - IR LED Emitter.
+ - BC547 Transistor.
+ - NodeMCU Board (ESP8266).
+ 
+ 
+## Instructions :page_facing_up:
+
+<img src="diagram.png" width="350">
+
+1. Assemble the components as shown in the diagram.
+2. Load 'pixmob-music-rhythm/pixmob-music-rhythm.ino'
+in the NodeMCU Board (ESP8266) using arduino IDE.
+
+### Note :pushpin:
+Make sure the KY-038 sensor is calibrated.
+
+
+
+## Authors
+
+- [@drneox](https://www.github.com/drneox)
+
---
+++ pixmob-music-rhythm/pixmob-music-rhythm.ino
@@ -0,0 +1,61 @@
+/* PixMob Music Rhythm 0.1v
+   Copyright (c) 2022, Carlos Ganoza Plasencia
+   url: http://carlosganoza.com
+   github.com/drneox
+
+*/
+
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+
+
+const int IRLedPin = 5;  // led GPIO pin
+const int microAnalog = A0; // analog pin for the sound sensor
+const int microDigital = 16; // digital pin for the sound sensor
+IRsend irsend(IRLedPin);
+
+/*
+   some colors codes
+*/
+uint16_t yellow[21] = {1400, 1400, 700, 700, 700, 1400, 700, 2800, 700, 2100, 1400, 700, 700, 700, 700, 1400, 1400, 2800, 1400, 2800, 700};
+uint16_t blue[27] =   {700, 700, 700, 2100, 1400, 700, 700, 2800, 700, 1400, 700, 700, 700, 1400, 1400, 700, 700, 1400, 700, 700, 700, 700, 700, 700, 700, 2100, 700};
+uint16_t green[21] = {1400, 1400, 700, 700, 700, 700, 1400, 2800, 700, 1400, 700, 1400, 700, 1400, 700, 1400, 1400, 2800, 1400, 2800, 700};
+uint16_t pink[23] = {700, 700, 700, 2100, 1400, 700, 700, 2800, 700, 1400, 700, 700, 700, 2800, 1400, 1400, 700, 2100, 700, 700, 700, 2100, 700};
+
+
+
+void setup() {
+  irsend.begin();
+  pinMode(microAnalog, INPUT);
+  pinMode(microDigital, INPUT);
+}
+
+void loop() {
+  int randomNumber = random(1, 4);
+
+  if (digitalRead(microDigital) == HIGH) {
+    if (randomNumber == 1) {
+      Serial.println("send yellow code");
+      irsend.sendRaw(yellow, 21, 38);
+    }
+    if (randomNumber == 2) {
+      Serial.println("send green code");
+      irsend.sendRaw(green, 21, 38);
+    }
+    if (randomNumber == 3) {
+      Serial.println("send pink code");
+      irsend.sendRaw(pink, 23, 38);
+    }
+    else {
+      Serial.println("send blue code");
+      irsend.sendRaw(blue, 27, 38);
+    }
+    delay(200);
+    Serial.println("-----");
+  }
+
+
+
+
+
+}
